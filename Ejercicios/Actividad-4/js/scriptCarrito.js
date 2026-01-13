0cefc93fa3aff74c5a14188b135a2fc3cd712048
mozonas <miguelozonas@gmail.com>
2026-01-13 12:24:30 +0100

    rev 13012026

--- Ejercicios/Actividad-4/js/scriptCarrito.js
+++ Ejercicios/Actividad-4/js/scriptCarrito.js
@@ -1,2 +1,84 @@
 'useStrict';
 console.log('Script de carrito cargado correctamente');
+const responseAPI= 
+{
+    "currency": "€",
+    "products": [
+      {
+        "SKU": "0K3QOSOV4V",
+        "title": "iFhone 13 Pro",
+        "price": "938.99"
+      },
+      {
+        "SKU": "TGD5XORY1L",
+        "title": "Cargador",
+        "price": "49.99"
+      },
+      {
+        "SKU": "IOKW9BQ9F3",
+        "title": "Funda de piel",
+        "price": "79.99"
+      },
+      {
+        "SKU": "IOKW9BQ9F4",
+        "title": "Funda de piel roja",
+        "price": "79.99"
+      },
+      {
+        "SKU": "IOKW9BQ9F5",
+        "title": "Funda de piel azul",
+        "price": "79.99"
+      },
+      {
+        "SKU": "IOKW9BQ9F6",
+        "title": "Funda de piel verde",
+        "price": "79.99"
+      }
+      ,
+      {
+        "SKU": "IOKW9BQ9F7",
+        "title": "Funda de piel amarilla",
+        "price": "79.99"
+      }
+    ]
+  };
+
+  const currency = responseAPI.currency;
+  const products = responseAPI.products;
+
+  function findSKU(SKU) {
+    return products.find(product => product.SKU === SKU);
+  }
+
+  function renderCartItems(products, containerSelector) {
+  const container = document.querySelector(containerSelector);
+  if (!container) return;
+
+  container.innerHTML = ''; // limpiar previo
+
+  products.forEach(product => {
+    const article = document.createElement('article');
+    article.className = 'cartItem';
+
+    article.innerHTML = `
+      <div class="cartItemInfo">
+        <h2 class="cartItemTitle">${product.title}</h2>
+        <p class="cartItemRef">Ref: ${product.SKU}</p>
+      </div>
+
+      <div class="cartItemQty">
+        <button class="cartItemBtn cartItemBtnMinus">-</button>
+        <span class="cartItemCount">0</span>
+        <button class="cartItemBtn cartItemBtnPlus">+</button>
+      </div>
+
+      <div class="cartItemUnitPrice">${product.price}€</div>
+      <div class="cartItemTotal">0 €</div>
+    `;
+
+    container.appendChild(article);
+  });
+}
+
+// uso
+renderCartItems(products, '.productsList');
